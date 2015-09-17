f406b635dd7581bdb93f57e6f4a96604e21dec0f
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2015-09-17 23:51:25 +0200

    Display event schedule

--- db/mappers/schedules_mapper.rb
+++ db/mappers/schedules_mapper.rb
@@ -1,4 +1,25 @@
 class SchedulesMapper < ROM::Mapper
   relation :schedules
   register_as :schedules
+
+  step do
+    ungroup entries: %w(title authors date start_time break)
+  end
+
+  step do
+    attribute :title, from: "title"
+    attribute :authors, from: "authors"
+    attribute :date, from: "date"
+    attribute :start_time, from: "start_time"
+    attribute :break, from: "break"
+  end
+
+  step do
+    exclude :conference
+    exclude :year
+  end
+
+  step do
+    group entries: %i(title authors start_time break)
+  end
 end
